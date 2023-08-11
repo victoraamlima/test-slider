64b95b422771cf34e740c2e1e424fa251e783c5f
Victor Lima <victoraamlima@gmail.com>
2023-08-10 21:03:57 -0300

    refactor

--- index.js
+++ index.js
@@ -1,58 +1,53 @@
 const slides = document.getElementsByClassName("slide")
+const leftArrow = document.getElementsByClassName("left-arrow")
+const rightArrow = document.getElementsByClassName("right-arrow")
 
 let currentSlide = 0
-
-let leftArrow = document.getElementsByClassName("left-arrow")
-let rightArrow = document.getElementsByClassName("right-arrow")
-
-function moveSlideRight() {
-    if (currentSlide === slides.length - 1) {
-        return
-    }
-
-    removeShow()
-
-    currentSlide++
-
-    addShow()
-
-    if (currentSlide > 0) {
-        leftArrow[0].classList.remove("opacity")
-        leftArrow[0].classList.add("hover")
-    }
-    
-    if (currentSlide === slides.length-1) {
-        rightArrow[0].classList.add("opacity")
-        rightArrow[0].classList.remove("hover")
-    }
-}
+const firstSlide = 0
+const lastSlide = slides.length - 1
 
 function addShow() {
-    slides[currentSlide].classList.add("show")
+  slides[currentSlide].classList.add("show")
 }
 
 function removeShow() {
-    slides[currentSlide].classList.remove("show")
+  slides[currentSlide].classList.remove("show")
+}
+
+function handleArrowsDisabled() {
+  if (currentSlide === firstSlide) {
+    leftArrow[0].classList.add("disabled")
+  } else {
+    leftArrow[0].classList.remove("disabled")
+  }
+
+  if (currentSlide === lastSlide) {
+    rightArrow[0].classList.add("disabled")
+  } else {
+    rightArrow[0].classList.remove("disabled")
+  }
+}
+
+function moveSlideRight() {
+  if (currentSlide === lastSlide) {
+    return
+  }
+
+  removeShow()
+  currentSlide++
+  addShow()
+
+  handleArrowsDisabled()
 }
 
 function moveSlideLeft() {
-    if (currentSlide === 0) {
-        return
-    }
+  if (currentSlide === firstSlide) {
+    return
+  }
 
-    removeShow()
+  removeShow()
+  currentSlide--
+  addShow()
 
-    currentSlide--
-
-    addShow()
-
-    if (currentSlide < 1) {
-        leftArrow[0].classList.add("opacity")
-        leftArrow[0].classList.remove("hover")
-    }
-
-    if (currentSlide < slides.length-1) {
-        rightArrow[0].classList.remove("opacity")
-        rightArrow[0].classList.add("hover")
-    }
+  handleArrowsDisabled()
 }
